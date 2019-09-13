5d653edf8748f77627470e3e6f8cc3948cf73c5a
Brian Park <briancpark@berkeley.edu>
2019-09-13 09:27:28 +0100

    fix

--- cell_auto.c
+++ cell_auto.c
@@ -108,7 +108,7 @@ int main(int argc, char ** argv)
     int width = (rows * 2 + 1);
     int length = (rows + 1);
 
-    printf("P1 %d %d ## %u rows of automata simulation (Rule %u)\n", width, length, rows, rule);
+    printf("P1 %c %c ## %u rows of automata simulation (Rule %u)\n", width, length, rows, rule);
 
     /*
      * PART 3: MEMORY MANAGEMENT
