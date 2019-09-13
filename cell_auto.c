aa8242f99638a0c2c12c8dabd903942715e3a620
Brian Park <briancpark@berkeley.edu>
2019-09-13 22:01:55 +0100

    fix

--- cell_auto.c
+++ cell_auto.c
@@ -165,7 +165,7 @@ int main(int argc, char ** argv)
         for (i = 2; i < width + 2; i++) {
             int loc = (rowB[i - 2] * 16) + (rowB[i - 1] * 8) + (rowB[i] * 4) + (rowB[i + 1] * 2) + rowB[i + 2];
 
-            if (((rule >> loc) & ((rule >> (loc + 1)) << 1)) != 0) {
+            if (((rule >> loc) & ((rule >> (loc + 1)) << 1)) == 0) {
                 rowA[i] = 1;
             }
         }
