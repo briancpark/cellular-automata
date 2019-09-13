2bf0031ed87571ee4d94d50ccb0579826c6b1215
Brian Park <briancpark@berkeley.edu>
2019-09-13 10:01:56 +0100

    fix

--- cell_auto.c
+++ cell_auto.c
@@ -162,7 +162,7 @@ int main(int argc, char ** argv)
         rowA = calloc(2 * rows + 5, 1);
 
         //Check the previous generation (rowB) and update the new generation (rowA)
-        for (i = 2; i < 2 * rows; i++) {
+        for (i = 2; i < 2 * rows + 3; i++) {
             int loc = (rowB[i - 2] * 16) + (rowB[i - 1] * 8) + (rowB[i] * 4) + (rowB[i + 1] * 2) + rowB[i + 2];
 
             if ((rule >> (loc)) - ((rule >> (loc + 1)) << 1) == 1) {
